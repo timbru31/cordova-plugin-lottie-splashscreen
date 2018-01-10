e71e83c86cde9e84ae70f28657b1019a32861342
Tim Brust <ratchet.player@gmx.de>
2018-01-10 11:12:26 +0100

    temp

--- www/lottie-splashscreen.ts
+++ www/lottie-splashscreen.ts
@@ -5,8 +5,8 @@ class LottieSplashscreen {
         exec(successCallback, errorCallback, 'LottieSplashscreen', 'hide', []);
     }
 
-    static show(successCallback: any, errorCallback: any) {
-        exec(successCallback, errorCallback, 'LottieSplashscreen', 'hide', []);
+    static show(location?: string, remote?: boolean, successCallback?: any, errorCallback?: any) {
+        exec(successCallback, errorCallback, 'LottieSplashscreen', 'show', [location, remote]);
     }
 }
 
