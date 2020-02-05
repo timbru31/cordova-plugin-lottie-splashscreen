3ae7ce3fbacfd99b0d85441f3230a90059b245d1
Tim Brust <tim.brust@sinnerschrader.com>
2020-02-05 11:33:34 +0100

    build: publish .d.ts files, too

---
+++ types/index.d.ts
@@ -0,0 +1,12 @@
+type LottieEvent = 'lottieAnimationStart' | 'lottieAnimationEnd' | 'lottieAnimationCancel' | 'lottieAnimationRepeat';
+
+interface LottieSplashScreen {
+    hide(): Promise<string>;
+    show(location?: string, remote?: boolean, width?: number, height?: number): Promise<string>;
+    on(event: LottieEvent, callback: (ev: Event) => void): void;
+    once(event: LottieEvent): Promise<unknown>;
+}
+
+declare const lottie: {
+    splashscreen: LottieSplashScreen;
+};
