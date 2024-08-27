e49edff61ec3cf9f177d96ec8dbe9790ee357765
Tim Brust <github@timbrust.de>
2024-08-27 19:56:20 +0200

    build(deps-dev): update ESLint to v9, typescript-eslint to v8

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
+
+export default tseslint.config({
+    files: ['**/*.ts'],
+    extends: [eslint.configs.recommended, ...tseslint.configs.recommended, eslintPluginPrettier],
+    rules: {
+        '@typescript-eslint/no-unused-vars': [
+            'error',
+            {
+                args: 'all',
+                argsIgnorePattern: '^_',
+                caughtErrors: 'all',
+                caughtErrorsIgnorePattern: '^_',
+                destructuredArrayIgnorePattern: '^_',
+                varsIgnorePattern: '^_',
+                ignoreRestSiblings: true,
+            },
+        ],
+        'prettier/prettier': [
+            'error',
+            {
+                endOfLine: 'auto',
+            },
+        ],
+    },
+});
